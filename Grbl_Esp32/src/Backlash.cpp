e7245c14f4d315f54152e78e7ca6e500e113c6d8
Nikos Siatras <nsiatras@gmail.com>
2023-01-01 15:56:50 +0200

    Backlash Compensation implemented

--- Grbl_Esp32/src/Backlash.cpp
+++ Grbl_Esp32/src/Backlash.cpp
@@ -1,8 +1,23 @@
-/**
- * Created by Nikos Siatras
- * Twitter: nsiatras
- * Website: https://www.sourcerabbit.com
- */
+/*
+  Backlash.cpp
+
+  Copyright (c) 2023 Nikolaos Siatras
+  Twitter: nsiatras
+  Website: https://www.sourcerabbit.com
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "Backlash.h"
 
@@ -37,10 +52,6 @@ void backlash_ini()
  */
 void backlash_compensate_backlash(float *target, plan_line_data_t *pl_data)
 {
-    /*char stringArray[10];
-      sprintf(stringArray, "%f", previous_targets[2]);
-      grbl_msg_sendf(CLIENT_SERIAL, MsgLevel::Info, stringArray);*/
-
     float backlash_compensation_target[MAX_N_AXIS] = {0.0};
     bool perform_backlash_compensation_motion = false;
 
--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,4 +1,4 @@
 #pragma once
 
 
-#include "Machines/ZX7045_4Axis.h"
+#include "Machines/sourcerabbit_Quantum.h"
