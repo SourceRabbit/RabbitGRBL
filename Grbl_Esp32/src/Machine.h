6b03701c0f84d814ccb4b1b5520a5e41aa1a1f71
Nikos Siatras <nsiatras@gmail.com>
2023-03-23 12:27:29 +0200

    Update Machine.h

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include "Machines/sourcerabbit_4axis_cnc_motherboard_1_2_Probe_As_LimitA.h"
-
 //#include "Machines/ZX7045_4Axis.h"
 
-//#include "Machines/sourcerabbit_Quantum.h"
+#include "Machines/sourcerabbit_Quantum.h"
 
 //#include "Machines/Custom.h"
