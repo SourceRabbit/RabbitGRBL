9276b22b6b67733f16164f7e68cae19475299370
Nikos Siatras <nsiatras@gmail.com>
2023-06-27 14:54:04 +0300

    4Axis CNC motherboard compact

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "Machines/SourceRabbit_4Axis_CNC_Motherboard_1_3.h"
+//#include "Machines/SourceRabbit_4Axis_CNC_Motherboard_1_3.h"
+#include "Machines/SourceRabbit_4Axis_CNC_Motherboard_Compact_1_1.h"
 
 //#include "Machines/SourceRabbit_Quantum.h"
 //#include "Machines/SourceRabbit_MicroMill.h"
