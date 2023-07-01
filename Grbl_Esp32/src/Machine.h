0379feba9da53e16ed590396932185614f070bb0
Nikos Siatras <nsiatras@gmail.com>
2023-07-01 15:15:30 +0300

    Machine changes

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,7 +1,9 @@
 #pragma once
 
-//#include "Machines/SourceRabbit_4Axis_CNC_Motherboard_1_3.h"
-#include "Machines/SourceRabbit_4Axis_CNC_Motherboard_Compact_1_1.h"
+//#include "Machines/4Axis_CNC_Motherboard_1_3.h"
+//#include "Machines/4Axis_CNC_Motherboard_Compact_1_1.h"
 
-//#include "Machines/SourceRabbit_Quantum.h"
-//#include "Machines/SourceRabbit_MicroMill.h"
+//#include "Machines/Quantum.h"
+//#include "Machines/MicroMill.h"
+//#include "Machines/RabbitMillMega_300x600x100.h"
+#include "Machines/Rabbit_Mill_Neo.h"
