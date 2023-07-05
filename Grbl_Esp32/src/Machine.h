9b48e821fb629da6f6e4e750d0b5db9e225e8373
Nikos Siatras <nsiatras@gmail.com>
2023-07-05 14:59:31 +0300

    Update Machine.h

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,9 +1,9 @@
 #pragma once
 
-//#include "Machines/4Axis_CNC_Motherboard_1_3.h"
+#include "Machines/4Axis_CNC_Motherboard_1_3.h"
 //#include "Machines/4Axis_CNC_Motherboard_Compact_1_1.h"
 
 //#include "Machines/Quantum.h"
 //#include "Machines/MicroMill.h"
 //#include "Machines/RabbitMillMega_300x600x100.h"
-#include "Machines/Rabbit_Mill_Neo.h"
+//#include "Machines/Rabbit_Mill_Neo.h"
