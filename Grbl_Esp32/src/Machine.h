892a98b42e4467da5a452d07fdc2c246ba32eeb7
Nikos Siatras <nsiatras@gmail.com>
2023-09-16 13:55:52 +0300

    Rabbit Mill Mega 400x800x100 config

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "Machines/4Axis_CNC_Motherboard_1_3.h"
+//#include "Machines/4Axis_CNC_Motherboard_1_3.h"
 //#include "Machines/4Axis_CNC_Motherboard_Compact_1_1.h"
 
 //#include "Machines/Quantum.h"
 //#include "Machines/MicroMill.h"
 //#include "Machines/RabbitMillMega_300x600x100.h"
+#include "Machines/RabbitMillMega_400x800x100.h"
 //#include "Machines/RabbitMill_Neo.h"
