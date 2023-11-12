2bb37aefdd874064950b8f1925ca5c2d37d77f32
Nikos Siatras <nsiatras@gmail.com>
2023-11-12 12:37:58 +0200

    Update Machine.h

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,14 +1,23 @@
 #pragma once
 
-
-#include "Machines/Custom/ZX7045_4Axis.h"
-
-//#include "Machines/4Axis_CNC_Motherboard_1_3.h"
-
+// SourceRabbit Controllers
+#include "Machines/4Axis_CNC_Motherboard_1_3.h"
 //#include "Machines/4Axis_CNC_Motherboard_Compact_1_1.h"
 
+// SourceRabbit Machines
 //#include "Machines/Quantum.h"
 //#include "Machines/MicroMill.h"
 //#include "Machines/RabbitMillMega_300x600x100.h"
 //#include "Machines/RabbitMillMega_400x800x100.h"
 //#include "Machines/RabbitMill_Neo.h"
+
+// Example Machines
+//#include "Machines/Examples/4Axis_CNC_Motherboard_1_3_ClonedY.h"
+
+// Other machines
+//#include "Machines/Custom/ZX7045_4Axis.h"
+
+
+
+
+
