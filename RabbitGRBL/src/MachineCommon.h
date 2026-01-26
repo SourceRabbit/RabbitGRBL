6e0cfea29bb49711dcbb7429998a1cd6c0f8cc5d
Nikos Siatras <nsiatras@gmail.com>
2026-01-26 11:09:26 +0200

    Update MachineCommon.h

--- RabbitGRBL/src/MachineCommon.h
+++ RabbitGRBL/src/MachineCommon.h
@@ -16,7 +16,6 @@
 
 // ESP32 CPU Settings
 // const uint32_t ESP32_TIMERS_CLOCK = 80000000; // For a classic ESP32, the LEDC timer clock is typically the 80 MHz APB clock.
-
 const uint32_t ESP32_TIMERS_CLOCK = getApbFrequency(); // For a classic ESP32, the LEDC timer clock is typically the 80 MHz APB clock.
 
 // =============== Don't change or comment these out ======================
