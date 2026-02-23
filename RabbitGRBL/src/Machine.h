1c6b2c0b357eb8e6651d8cf54a29ffeb18993e88
Nikos Siatras <nsiatras@gmail.com>
2026-02-23 23:22:03 +0200

    Update Machine.h

--- RabbitGRBL/src/Machine.h
+++ RabbitGRBL/src/Machine.h
@@ -1,12 +1,8 @@
 #pragma once
 
 // SourceRabbit Motion Controllers
-//#include "Machines/MotionControllers/SourceRabbit/4axis_cnc_motherboard_1_3.h"
 //#include "Machines/MotionControllers/SourceRabbit/rabbit_board_4axis_1_2.h"
-
-// Other
-//#include "Machines/MotionControllers/Other/espduino_cnc_shieldv3.h"
-
+//#include "Machines/MotionControllers/SourceRabbit/4axis_cnc_motherboard_1_3.h"
 
 // SourceRabbit Machines
 //#include "Machines/CNCMachines/SourceRabbit/Quantum.h"
@@ -15,7 +11,11 @@
 //#include "Machines/CNCMachines/SourceRabbit/RabbitMillMega_400x800x100.h"
 //#include "Machines/CNCMachines/SourceRabbit/RabbitMill_Neo.h"
 
+// Genmitsu Machines
 #include "Machines/CNCMachines/Genmitsu/3030_prover_rabbit_board_4axis_1_2.h"
 
-// Other machines
+// Other Boards
+//#include "Machines/MotionControllers/Other/espduino_cnc_shieldv3.h"
+
+// Custom Machines
 //#include "Machines/CNCMachines/Other/zx7045_4axis.h"
