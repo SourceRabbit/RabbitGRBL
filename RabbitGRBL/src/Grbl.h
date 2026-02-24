cccf2b2fd1bbe6364eed338f40f37e63c6f9f37e
Nikos Siatras <nsiatras@gmail.com>
2026-02-24 23:46:22 +0200

    Version 1.5.4 Build 20260224

--- RabbitGRBL/src/Grbl.h
+++ RabbitGRBL/src/Grbl.h
@@ -21,7 +21,7 @@
 */
 
 // Grbl versioning system
-const char *const FIRMWARE_NAME = "Rabbit GRBL v1.5.3";
+const char *const FIRMWARE_NAME = "Rabbit GRBL v1.5.4";
 const char *const GRBL_VERSION_BUILD = "20260224";
 const char *const GRBL_VERSION = "1.1h";
 
