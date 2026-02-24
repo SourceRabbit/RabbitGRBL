3f4ff609b2c88956548cec0d81fcdad361062c8c
Nikos Siatras <nsiatras@gmail.com>
2026-02-24 10:44:02 +0200

    StandardStepper removed

--- RabbitGRBL/src/Motors/StandardStepper.cpp
+++
@@ -1,8 +0,0 @@
-/*
-    StandardStepper.cpp
-
-    This file is kept for backwards compatibility.
-    The implementation has been split into:
-      - Stepper_RMT.cpp  (for USE_RMT_STEPS builds)
-      - Stepper_Software.cpp  (for non-RMT builds)
-*/
--- RabbitGRBL/src/Motors/StandardStepper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef USE_RMT_STEPS
-#include "Stepper_RMT.h"
-#else
-#include "Stepper_Software.h"
-#endif
