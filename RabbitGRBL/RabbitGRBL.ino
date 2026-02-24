900952fb704593c5040156916ae7c2f460673eb3
Nikos Siatras <nsiatras@gmail.com>
2026-02-24 22:30:21 +0200

    Update RabbitGRBL.ino

--- RabbitGRBL/RabbitGRBL.ino
+++ RabbitGRBL/RabbitGRBL.ino
@@ -2,6 +2,9 @@
 
 /*
   Builds Correctly with v2.0.0 ESP32 Board Package for Arduino
+
+  For help use the WikiPage 
+  https://github.com/SourceRabbit/RabbitGRBL/wiki/How-to-Compile-with-Arduino-IDE
 */
 
 #include "src/Grbl.h"
