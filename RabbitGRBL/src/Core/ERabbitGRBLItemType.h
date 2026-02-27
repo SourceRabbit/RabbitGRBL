abbd9f8a4472abb1472e46781ec861fa32e95d20
Nikos Siatras <nsiatras@gmail.com>
2026-02-27 11:39:38 +0200

    Update ERabbitGRBLItemType.h

--- RabbitGRBL/src/Core/ERabbitGRBLItemType.h
+++ RabbitGRBL/src/Core/ERabbitGRBLItemType.h
@@ -5,6 +5,6 @@
 enum class ERabbitGRBLItemType : uint8_t
 {
     GRBL = 1,     // Classic GRBL settings like $100
-    EXTENDED = 2, // Settings added by early versions of Grbl_Esp32
+    EXTENDED = 2, // Extended Settings
     GRBLCMD = 3   // Non-persistent GRBL commands like $H
 };
